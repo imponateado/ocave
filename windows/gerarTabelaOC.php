ab70834970d3533c00b9b9123a36e852a37ab74b
LEONARDO SILVA TEODORO <leonardo.teodoro@cs.udf.edu.br>
2024-11-11 18:30:16 -0300

    I don't think I've changed anything

--- windows/gerarTabelaOC.php
+++ windows/gerarTabelaOC.php
@@ -7,31 +7,12 @@
         <?php require '../functions/navbar.php'; ?>
         <!-- Page Content  -->
         <div id="content" class="p-4 p-md-5 pt-5">
-
-            <?php require '../functions/startAndEndDate.php' ?>
-
-            <?php require '../functions/getRotaListHTML.php' ?>
-
-            <button class="btn btn-success" onclick="onButtonClick()">Consultar</button>
-            <div id="contentHistory"></div>
-
-            <div>Opa... Gerar tabelas a partir de ordens de carga ainda não estão implementadas! (provavelmente descartado) </div>
+            <div>Opa... Gerar tabelas a partir de ordens de carga ainda não estão implementadas! (provavelmente
+                descartado) </div>
         </div>
     </div>
-    
+
     <?php require '../functions/scripts.php'; ?>
-
-    <script>
-        function onButtonClick() {
-            const startDate = document.getElementById('startDate').value;
-            const endDate = document.getElementById('endDate').value;
-            const rota = document.getElementById('rotas').value;
-        }
-
-        window.onload = function () {
-            <?php require '../functions/getRotaListHTMLscript.php'; ?>
-        }
-    </script>
 </body>
 
 </html>
